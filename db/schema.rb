5a4ef4104acbe1a1f14367bcd242daf4eb92a7e1
mike43 <mike.morris.wa@gmail.com>
2015-11-24 03:54:55 +0000

    Add Stripe functionality to sign-up

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151122065326) do
+ActiveRecord::Schema.define(version: 20151123120309) do
+
+  create_table "payments", force: :cascade do |t|
+    t.string   "email"
+    t.string   "token"
+    t.string   "user_id"
+    t.string   "integer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
